7252338569e0508e2a55585993dfbf2454a3c756
PauloNuhaj <paulonuhaj@gmail.com>
2026-03-06 10:50:02 +0100

    Ndryshimet ne css per pamjen ne mobile

--- script.js
+++ script.js
@@ -17,100 +17,127 @@ document.querySelectorAll('.nav-menu ul li a').forEach(link => {
 
 // Funksioni për player-at audio
 const sounds = [
-    // Kanale ekzistuese
     new Howl({ src: ['https://cp1.sednastream.com/proxy/radioone94?mp=/stream'], html5: true }),
     new Howl({ src: ['https://s4.radio.co/sd1d1904dc/listen'], html5: true }),
     new Howl({ src: ['https://cp1.sednastream.com/proxy/clubfm?mp=/stream'], html5: true }),
     new Howl({ src: ['https://live.topgold.al/tar'], html5: true }),
     new Howl({ src: ['https://live.topgold.al/topgold'], html5: true }),
 
-    // Kanale të reja
-    new Howl({ src: ['https://live.top-media.al/mmr'], html5: true }), // My Music
-    new Howl({ src: ['https://cp1.sednastream.com/proxy/radiotravel?mp=/stream'], html5: true }), // Radio Travel
-    new Howl({ src: ['https://eu8.fastcast4u.com/proxy/chillradio?mp=/1'], html5: true }), // Chill Radio
-    new Howl({ src: ['https://antena.albaniadigitalradio.al/loveradio'], html5: true }) // Love Radio
+    new Howl({ src: ['https://live.top-media.al/mmr'], html5: true }),
+    new Howl({ src: ['https://cp1.sednastream.com/proxy/radiotravel?mp=/stream'], html5: true }),
+    new Howl({ src: ['https://eu8.fastcast4u.com/proxy/chillradio?mp=/1'], html5: true }),
+    new Howl({ src: ['https://antena.albaniadigitalradio.al/loveradio'], html5: true })
 ];
 
 let currentSoundIndex = -1;
 
-document.querySelectorAll('.audio-player').forEach((player, index) => {
-    const playPauseButton = player.querySelector('.play-pause');
-    const volumeControl = player.querySelector('.volume');
-
-    playPauseButton.addEventListener('click', () => {
-        if (currentSoundIndex === index && sounds[index].playing()) {
-            sounds[index].pause();
-            playPauseButton.textContent = 'Play';
-        } else {
-            if (currentSoundIndex !== -1 && sounds[currentSoundIndex].playing()) {
-                sounds[currentSoundIndex].pause();
-                document.querySelectorAll('.play-pause')[currentSoundIndex].textContent = 'Play';
-            }
-            sounds[index].play();
-            playPauseButton.textContent = 'Pause';
-            currentSoundIndex = index;
-        }
-    });
-
-    volumeControl.addEventListener('input', () => {
-        sounds[index].volume(volumeControl.value);
-    });
-});
-
-// Butoni Scroll-to-Top
-window.addEventListener('scroll', function() {
-    const scrollToTopButton = document.getElementById('scrollToTop');
-    if (window.scrollY > 300) {
-        scrollToTopButton.style.display = 'block';
-    } else {
-        scrollToTopButton.style.display = 'none';
-    }
-});
-
-document.getElementById('scrollToTop').addEventListener('click', function() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
-
-// Fshij loader-in kur faqja të ngarkohet
-window.addEventListener('load', function() {
-    document.querySelector('.loader').style.display = 'none';
-});
-
-// Kontrolli i audio player-it global
 const globalPlayPauseButton = document.getElementById('play-pause');
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
 const globalIcon = globalPlayPauseButton.querySelector('i');
 
-prevButton.addEventListener('click', () => {
-    if (currentSoundIndex > 0) {
-        const newIndex = currentSoundIndex - 1;
-        document.querySelectorAll('.play-pause')[newIndex].click();
-    }
-});
+const channelButtons = document.querySelectorAll('.play-pause');
 
-nextButton.addEventListener('click', () => {
-    if (currentSoundIndex < sounds.length - 1) {
-        const newIndex = currentSoundIndex + 1;
-        document.querySelectorAll('.play-pause')[newIndex].click();
-    }
-});
+document.querySelectorAll('.audio-player').forEach((player, index) => {
 
-globalPlayPauseButton.addEventListener('click', () => {
-    if (currentSoundIndex !== -1) {
+    const playPauseButton = player.querySelector('.play-pause');
+    const volumeControl = player.querySelector('.volume');
 
-        if (sounds[currentSoundIndex].playing()) {
-            sounds[currentSoundIndex].pause();
+    playPauseButton.addEventListener('click', () => {
+
+        if (currentSoundIndex === index && sounds[index].playing()) {
+
+            sounds[index].pause();
+            playPauseButton.textContent = 'Play';
+
             globalIcon.classList.remove('fa-pause');
             globalIcon.classList.add('fa-play');
+
         } else {
-            sounds[currentSoundIndex].play();
+
+            if (currentSoundIndex !== -1 && sounds[currentSoundIndex].playing()) {
+
+                sounds[currentSoundIndex].pause();
+                channelButtons[currentSoundIndex].textContent = 'Play';
+
+            }
+
+            sounds[index].play();
+            playPauseButton.textContent = 'Pause';
+
+            currentSoundIndex = index;
+
             globalIcon.classList.remove('fa-play');
             globalIcon.classList.add('fa-pause');
         }
 
+    });
+
+    volumeControl.addEventListener('input', () => {
+        sounds[index].volume(volumeControl.value);
+    });
+
+});
+
+// Butoni Scroll-to-Top
+window.addEventListener('scroll', function() {
+
+    const scrollToTopButton = document.getElementById('scrollToTop');
+
+    if (window.scrollY > 300) {
+        scrollToTopButton.style.display = 'block';
+    } else {
+        scrollToTopButton.style.display = 'none';
     }
+
+});
+
+document.getElementById('scrollToTop').addEventListener('click', function() {
+
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+
+});
+
+// Fshij loader-in kur faqja të ngarkohet
+window.addEventListener('load', function() {
+
+    document.querySelector('.loader').style.display = 'none';
+
+});
+
+// Kontrolli i audio player-it global
+
+prevButton.addEventListener('click', () => {
+
+    if (currentSoundIndex > 0) {
+
+        const newIndex = currentSoundIndex - 1;
+        channelButtons[newIndex].click();
+
+    }
+
+});
+
+nextButton.addEventListener('click', () => {
+
+    if (currentSoundIndex < sounds.length - 1) {
+
+        const newIndex = currentSoundIndex + 1;
+        channelButtons[newIndex].click();
+
+    }
+
+});
+
+globalPlayPauseButton.addEventListener('click', () => {
+
+    if (currentSoundIndex !== -1) {
+
+        channelButtons[currentSoundIndex].click();
+
+    }
+
 });
